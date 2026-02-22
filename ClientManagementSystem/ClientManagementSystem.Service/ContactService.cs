dfb068ad9c8e80838be3354d2539f444678619b5
Siphenathi <spantshwa.lukho@gmail.com>
2026-02-23 00:42:29 +0200

    Moved files

--- ClientManagementSystem/ClientManagementSystem.Service/ContactService.cs
+++ ClientManagementSystem/ClientManagementSystem.Service/ContactService.cs
@@ -142,12 +142,4 @@ namespace ClientManagementSystem.Service
 			};
 		}
 	}
-
-    public interface IContactService
-    {
-	    Task<IEnumerable<DisplayContact>> GetAllContactsAsync(bool includeDeletedRecords = false);
-		Task<string> CreateContactAsync(CreateContactRequest createContactRequest);
-		Task<string> CreateContactClientsAsync(LinkClientsToContact linkClientsToContact);
-		Task<LinkClientsToContact> GetContactClients(int contactId);
-    }
 }
---
+++ ClientManagementSystem/ClientManagementSystem.Service/IContactService.cs
@@ -0,0 +1,11 @@
+﻿using ClientManagementSystem.Model;
+
+namespace ClientManagementSystem.Service;
+
+public interface IContactService
+{
+	Task<IEnumerable<DisplayContact>> GetAllContactsAsync(bool includeDeletedRecords = false);
+	Task<string> CreateContactAsync(CreateContactRequest createContactRequest);
+	Task<string> CreateContactClientsAsync(LinkClientsToContact linkClientsToContact);
+	Task<LinkClientsToContact> GetContactClients(int contactId);
+}
